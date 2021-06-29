2c81b8f74141e6627bd4b6053a34c8b3b67a3a10
didija <jassmine135@gmail.com>
2021-06-29 03:52:09 +0100

    jj

---
+++ scriptphp/connexion.php
@@ -0,0 +1,19 @@
+<?php
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            
+            //On établit la connexion
+            $connect= mysqli_connect("localhost","root","","lifestyle");
+            
+            //On vérifie la connexion
+            if($connect->connect_error){
+                die('Erreur : ' .$connect->connect_error);
+            }
+            else{
+                echo 'Connexion réussie';
+            }
+            
+            
+            
+ ?>
---
+++ scriptphp/inscription.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'connexion.php';
+
+$fstname=$_POST['fstname'];
+$lstname=$_POST['lstname'];
+$pass=$_POST['pass'];
+$email=$_POST['email'];
+$birthday=$_POST['birthday'];
+$height=$_POST['height'];
+$weight=$_POST['weight'];
+$sexe=$_POST['sexe'];
+ echo $sexe;
+
+
+    if(!empty($fstname) && !empty($lstname) && !empty($pass)&& !empty($email)&&!empty($birthday)&&!empty($height)&&!empty($weight)&&!empty($sexe)){
+        $sql = "INSERT INTO client (first_name, last_name, birthday, sexe, taille, poids, email, _PASSWORD)
+        VALUES ('".$fstname."','".$lstname."','".$birthday."','".$sexe."','".$height."','".$weight."','".$email."','".$pass."')";
+        mysqli_query($connect,$sql);
+
+        header('Location: ../service.html');
+        
+    }
+    else{
+        echo"error";
+    }
+  
+    if ($sql ==1 ) {
+       
+        exit();
+     }
+    
